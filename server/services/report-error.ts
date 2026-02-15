cde8171e0d3859e1c0f83ee678269794462e70af
Charles <zekvelasco15@gmail.com>
2026-02-15 23:21:32 +0800

    feat: added functionality to send logs and errors to better stack as well as screenshots of test failures to supabase storage

---
+++ server/services/report-error.ts
@@ -0,0 +1,31 @@
+import type { TestInfo } from '@playwright/test';
+import logger from '../lib/pino';
+import initSentry from 'server/lib/sentry';
+import uploadScreenshot from './upload-screenshot';
+/**
+ * Logs to Better Stack Logs, reports error to Better Stack Errors and uploads screenshot to Supabase Storage.
+ * @param testInfo - The test information.
+ */
+export default async function logAndReportError(
+  testInfo: TestInfo,
+  screenshotBuffer: Buffer,
+  explicitError?: Error,
+): Promise<void> {
+  const Sentry = initSentry();
+
+  if (testInfo.status !== 'passed' || explicitError) {
+    const error = explicitError || testInfo.error;
+    const errorMessage = `Test failed: ${testInfo.title}`;
+
+    logger.error(errorMessage);
+    if (error) {
+      logger.error(`Error: ${error.message}`);
+      logger.error(`Stack: ${error.stack}`);
+    }
+
+    Sentry.captureException(error);
+
+    // upload the screenshot to supabase
+    await uploadScreenshot(screenshotBuffer, testInfo.title);
+  }
+}
---
+++ server/services/upload-screenshot.ts
@@ -0,0 +1,35 @@
+import { supabase } from 'server/lib/supabase';
+import logger from 'server/lib/pino';
+
+/**
+ * Uploads a screenshot to Supabase Storage.
+ * @param screenshotBuffer - The screenshot buffer to upload.
+ * @param testTitle - The title of the test.
+ */
+export default async function uploadScreenshot(
+  screenshotBuffer: Buffer,
+  testTitle: string,
+): Promise<void> {
+  const sanitizedTitle = testTitle.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+  const filePath = `failures/${new Date().toISOString().toLocaleString()}_${sanitizedTitle}.png`;
+
+  const { data, error } = await supabase.storage
+    .from('test-failures')
+    .upload(filePath, screenshotBuffer, {
+      contentType: 'image/png',
+      upsert: false,
+    });
+
+  if (error) {
+    logger.error(error);
+    throw new Error(error.message);
+  }
+
+  const { data: publicUrlData } = supabase.storage
+    .from('test-failures')
+    .getPublicUrl(filePath);
+
+  logger.info(
+    `new screenshot of test failure uploaded to: ${publicUrlData.publicUrl}`,
+  );
+}
