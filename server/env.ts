73785d7f7e4a63e95e841bd079b5e857e3b0f45b
Charles <zekvelasco15@gmail.com>
2026-02-15 16:32:43 +0800

    chore: added .env validation

---
+++ server/env.ts
@@ -0,0 +1,19 @@
+import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod/v4';
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(['development', 'production']).default('development'),
+    PORT: z.coerce.number(),
+
+    BETTER_STACK_ERROR_DSN: z.url(),
+    BETTER_STACK_ERROR_TOKEN: z.string(),
+    BETTER_STACK_LOGS_DSN: z.url(),
+    BETTER_STACK_LOGS_TOKEN: z.string(),
+
+    APP_URL: z.url(),
+  },
+  runtimeEnv: process.env,
+  skipValidation:
+    !!process.env.CI || process.env.npm_lifecycle_event === 'lint',
+});
