51aedfe54572b69e689c7a67d8d21b31742003a5
Charles <zekvelasco15@gmail.com>
2026-02-19 04:31:02 +0800

    feat: added user checkout test

--- server/tests/e2e/shop.test.ts
+++ server/tests/e2e/shop.test.ts
@@ -110,4 +110,87 @@ test.describe('Shop Page', () => {
       await expect(page.getByRole('img', { name: product.name })).toBeVisible();
     }
   });
+
+  test('checkout should be partially successful', async ({ page }) => {
+    await page.waitForLoadState('domcontentloaded');
+    // select/search for items and add to cart
+    // first product
+    await page.getByRole('button', { name: 'Featured' }).click();
+    await page.getByRole('menuitem', { name: 'Price - Low to High' }).click();
+    await page.getByText('Meal Prep Made Simple').click();
+    await page.getByRole('button', { name: ' Add to cart' }).click();
+
+    // second product
+    await page.getByRole('link', { name: 'All Products' }).click();
+    await page.getByRole('button', { name: 'Featured' }).click();
+    await page.getByRole('menuitem', { name: 'Price - High to Low' }).click();
+    await page.getByText('Office Lunch Box').click();
+    await page.getByRole('button', { name: ' Add to cart' }).click();
+
+    // third product
+    await page.getByRole('link', { name: 'All Products' }).click();
+    await page.getByRole('button', { name: 'Featured' }).click();
+    await page.getByRole('menuitem', { name: 'Name (A-Z)' }).click();
+    await page.getByText("Athlete's Power Pack").click();
+    await page.getByRole('button', { name: ' Add to cart' }).click();
+
+    // view cart
+    await page.getByRole('button', { name: 'View cart' }).click();
+    await expect(page.locator('#order_total')).toContainText('$ 181.70');
+    await page.getByRole('button', { name: 'Checkout ' }).click();
+
+    // fill in checkout from
+    await page.locator('input[name="name"]').fill('John Doe');
+    await page.locator('input[name="email"]').fill('john.doe@gmail.com');
+    await page.locator('input[name="phone"]').fill('89898989');
+    await page.locator('input[name="company_name"]').fill('Microsoft');
+    await page.locator('input[name="vat"]').fill('test');
+    await page.locator('input[name="street"]').fill('Redmond, Washington');
+    await page.locator('input[name="street2"]').fill('Presidential Suite');
+    await page.locator('input[name="street"]').click();
+    await page.locator('input[name="street"]').fill('10 Bayfront Avenue');
+    await page.locator('input[name="company_name"]').fill('Marina Bay Sands');
+    await page.locator('input[name="city"]').fill('Singapore');
+    await page.locator('input[name="zip"]').fill('100000');
+    await page.getByLabel('Country').selectOption('197');
+    await page.getByRole('button', { name: 'Continue checkout ' }).click();
+
+    // payment
+    await expect(page.locator('#shipping_and_billing')).toContainText(
+      '10 Bayfront Avenue, Presidential Suite, Singapore 100000, Singapore',
+    );
+    await page
+      .getByRole('textbox', { name: 'Payment Details (test data)' })
+      .fill('8989 8989 8989 8989');
+    await page
+      .getByRole('checkbox', { name: 'Save my payment details' })
+      .check();
+    await page
+      .getByRole('button', { name: 'Order summary 3  item(s) -  $' })
+      .click();
+    await expect(
+      page.getByRole('img', { name: 'Meal Prep Made Simple' }),
+    ).toBeVisible();
+    await expect(page.locator('#cart_products')).toContainText(
+      '1 x Meal Prep Made Simple',
+    );
+    await expect(page.locator('#cart_products')).toContainText('$ 39.00');
+    await expect(
+      page.getByRole('img', { name: 'Office Lunch Box' }),
+    ).toBeVisible();
+    await expect(page.locator('#cart_products')).toContainText(
+      '1 x Office Lunch Box',
+    );
+    await expect(page.locator('#cart_products')).toContainText('$ 70.00');
+    await expect(
+      page.getByRole('img', { name: "Athlete's Power Pack" }),
+    ).toBeVisible();
+    await expect(page.locator('#cart_products')).toContainText(
+      "1 x Athlete's Power Pack",
+    );
+    await expect(page.locator('#cart_products')).toContainText('$ 49.00');
+    await expect(page.locator('#order_total')).toContainText('$ 181.70');
+    await page.getByRole('button', { name: 'Pay now' }).click();
+    // end of checkout, no handler for payments
+  });
 });
